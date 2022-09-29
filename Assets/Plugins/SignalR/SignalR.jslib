9cf04590db6bedb3a0949a643ca54eb4a871fa3a
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-09-28 21:04:39 -0500

    It works!!

--- Assets/Plugins/SignalR/SignalR.jslib
+++ Assets/Plugins/SignalR/SignalR.jslib
@@ -6,6 +6,21 @@ mergeInto(LibraryManager.library, {
 
   AddNumbers: function (x, y) {
     return x + y;
-  }
+  },
+  
+  ConnectJs: function(gameCode, url) {
+	console.log('Called ConnectJs');
+	window.HtConnect(UTF8ToString(gameCode), UTF8ToString(url));
+  },
+  
+  StopJs: function() {
+	console.log('Called StopJs');
+	window.HtStop();
+  },
+  
+  DisposeJs: function() {
+	console.log('Called DisposeJs');
+	window.HtDispose();
+  },
 
 });
--- Assets/Scripts/Hub/HtHubConnectionWebGl.cs
+++ Assets/Scripts/Hub/HtHubConnectionWebGl.cs
@@ -3,41 +3,105 @@
 using Assets.Scripts.Models;
 using Assets.Scripts.ServerModels;
 using System;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace Assets.Scripts.Hub
 {
     public class HtHubConnectionWebGl : IHtHubConnection
     {
-        
         public event EventHandler<HmCommandModel> OnHmCommand;
         public event EventHandler<HmCommandPredefinedModel> OnHmPredefinedCommand;
         public event EventHandler<HubConnectionStatusEventArgs> OnConnectionStatusChanged;
 
+        [DllImport("__Internal")]
+        //private static extern void ConnectJs(string gameCode, string url, Action<string> connectionEventCallback, Action<string, string> onCallbacks);
+        private static extern void ConnectJs(string gameCode, string url);
+        [DllImport("__Internal")]
+        private static extern void StopJs();
+        [DllImport("__Internal")]
+        private static extern void DisposeJs();
+
+        readonly GameCodeModel gameCodeModel;
 
         public HtHubConnectionWebGl(GameCodeModel gameCodeModel)
         {
-            
+            this.gameCodeModel = gameCodeModel;
         }
 
-        public async Task<bool> ConnectAsync()
+        void OnConnectionEventCallback(string data)
         {
-            return false;    
+            Debug.Log($"Data: {data}");
         }
 
-        public async Task StopAsync()
+        void OnOnCallBacks(string type, string data)
         {
-            
+            Debug.Log($"Type: {type} Data: {data}");
         }
 
-        public async Task PlayerSendLog(TextLogModel model)
+        public Task<bool> ConnectAsync()
         {
-            
+            Debug.Log($"Called {nameof(ConnectAsync)}");
+            try
+            {
+                // we will assume it connects
+                //ConnectJs(gameCodeModel.GameCode, Constants.HubUrl, OnConnectionEventCallback, OnOnCallBacks);
+                ConnectJs(gameCodeModel.GameCode, Constants.HubUrl);
+                return Task.FromResult(true);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("(Hub) Failed to connect: " + e.ToString());
+                ConnectionStatusChanged(new HubConnectionStatusEventArgs()
+                {
+                    FailedToConnect = true,
+                    Exception = e
+                });
+                return Task.FromResult(false);
+            }
         }
 
-        public async Task DisposeAsync()
+        private void ConnectionStatusChanged(HubConnectionStatusEventArgs args)
         {
+            var ev = OnConnectionStatusChanged;
+            ev?.Invoke(null, args);
+        }
+
+        public Task StopAsync()
+        {
+            Debug.Log($"Called {nameof(StopAsync)}");
+            try
+            {
+                StopJs();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("(Hub) Failed to stop: " + e.ToString());
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task PlayerSendLog(TextLogModel model)
+        {
+            // Quit message is sent through StopJs
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            Debug.Log($"Called {nameof(DisposeAsync)}");
+            try
+            {
+                DisposeJs();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError("(Hub) Failed to dispose: " + e.ToString());
+            }
             
+            return Task.CompletedTask;
         }
     }
 }
