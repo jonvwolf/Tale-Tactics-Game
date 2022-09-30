f62804f54daf181df81dfcc97ccb044ce5210dbe
jonwolfdev <49768794+jonwolfdev@users.noreply.github.com>
2022-09-29 20:25:50 -0500

    Fix jslink

--- Assets/Scripts/GameScene/JsLinkController.cs
+++ Assets/Scripts/GameScene/JsLinkController.cs
@@ -1,17 +1,46 @@
 using Assets.Scripts;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class JsLinkController : MonoBehaviour
 {
-    public void InvokeEvent(string type, string data)
+    /// <summary>
+    /// For this to work, create a gameobject (empty 2d) in the scene named `JsLink`
+    /// Then attach this monobehaviour to the game object, thats it
+    /// (In your outside js get the unity game reference and call it example: 
+    /// `_window.HtUnityInstance.SendMessage('JsLink', 'InvokeEvent', 'type test', 'hi from js');`
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="data"></param>
+    public void InvokeEvent(string data)
     {
-        Debug.Log($"Called InvokeEvent: {type} Data: {data}");
+        Debug.Log($"Called InvokeEvent Data: {data}");
+
+        // data must be of this format (without quotes)
+        // "/*TYPE_GOES_HERE*/{...json...}"
+
+        if (string.IsNullOrWhiteSpace(data) || data[0] != '/' || data[1] != '*')
+        {
+            Debug.LogError("Wrong format for data");
+            return;
+        }
+
+        var sb = new StringBuilder(22);
+        for (int i = 2; i < data.Length; i++)
+        {
+            if (data[i] == '*')
+            {
+                break;
+            }
+            sb.Append(data[i]);
+        }
+
         Global.JsLinkEventArgs(new Assets.Scripts.Models.JsLinkEventArgs()
         {
             Data = data,
-            Type = type
+            Type = sb.ToString(),
         });
     }
 }
